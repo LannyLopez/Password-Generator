29c50c059d5728bafbc8aad8b94a9838aed99985
lanny lopez <lannylynnlopez89@gmail.com>
2021-10-03 01:06:40 -0600

    made it so if invalid amount returns to nothing

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,7 @@
 // Assignment code here
 var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R", "S","T","U","V","W","X","Y","Z"];
-var numbers = [1,2,3,4,5,6,7,8,9,0];
+var aNumbers = [1,2,3,4,5,6,7,8,9,0];
 var specialCharacters = ["!","@","#","$","%","&","*"];
 
 
@@ -9,22 +9,22 @@ function generatePassword(){
 var passLength = window.prompt("Pick between 8-128 for a password")
  if(passLength <= 7 || passLength >= 129){
    window.alert ("Please pick a valid option")
-   generatePassword()
+  return 
    
  }
 
- window.confirm("Would you like Uppercase Characters")
+ var upCase = window.confirm("Would you like Uppercase Characters");
+ 
 
-
- window.confirm("Would you like Lowercase Characters")
-
-
-
- window.confirm("Would you like Numbers")
+ var lowerCase = window.confirm("Would you like Lowercase Characters")
 
 
 
- window.confirm("Would you like Special Characters")
+ var Numz = window.confirm("Would you like Numbers")
+
+
+
+ var sCharacters = window.confirm("Would you like Special Characters")
 
 
 
